91ebe160e76e00b2015ce097f919af76d36a6faf
Danilo Dominguez <danilo.dope@gmail.com>
2025-06-27 11:22:59 -0500

    Add DAO and repository methods to load leaderboards data

--- app/src/main/java/com/nqueens/game/core/data/database/daos/NQueensGamesWonDao.kt
+++ app/src/main/java/com/nqueens/game/core/data/database/daos/NQueensGamesWonDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.nqueens.game.core.data.database.entities.NQueensGamesWon
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface NQueensGamesWonDao {
@@ -15,12 +16,39 @@ interface NQueensGamesWonDao {
     fun getAllGames(): List<NQueensGamesWon>
 
     @Query(
+        "SELECT * FROM ${NQueensGamesWon.TABLE_NAME} ORDER BY ${NQueensGamesWon.COLUMN_DATE} DESC LIMIT :limit OFFSET :offset",
+    )
+    fun getAllGamesOffsetFlow(
+        offset: Int,
+        limit: Int,
+    ): Flow<List<NQueensGamesWon>>
+
+    @Query(
         "SELECT * FROM ${NQueensGamesWon.TABLE_NAME} WHERE ${NQueensGamesWon.COLUMN_PLAYER_NAME} = :playerName ORDER BY ${NQueensGamesWon.COLUMN_DATE} DESC",
     )
     fun getGamesByPlayer(playerName: String): List<NQueensGamesWon>
 
     @Query(
+        "SELECT DISTINCT ${NQueensGamesWon.COLUMN_QUEENS_COUNT} FROM ${NQueensGamesWon.TABLE_NAME} ORDER BY ${NQueensGamesWon.COLUMN_QUEENS_COUNT} ASC",
+    )
+    fun getDistinctQueensCountsFlow(): Flow<List<Int>>
+
+    @Query(
+        "SELECT DISTINCT ${NQueensGamesWon.COLUMN_QUEENS_COUNT} FROM ${NQueensGamesWon.TABLE_NAME} ORDER BY ${NQueensGamesWon.COLUMN_QUEENS_COUNT} ASC",
+    )
+    fun getDistinctQueensCounts(): List<Int>
+
+    @Query(
         "SELECT * FROM ${NQueensGamesWon.TABLE_NAME} WHERE ${NQueensGamesWon.COLUMN_QUEENS_COUNT} = :queensCount ORDER BY ${NQueensGamesWon.COLUMN_DATE} DESC",
     )
     fun getGamesByQueensCount(queensCount: Int): List<NQueensGamesWon>
+
+    @Query(
+        "SELECT * FROM ${NQueensGamesWon.TABLE_NAME} WHERE ${NQueensGamesWon.COLUMN_QUEENS_COUNT} = :queensCount ORDER BY ${NQueensGamesWon.COLUMN_DATE} DESC LIMIT :limit OFFSET :offset",
+    )
+    fun getGamesOffsetByQueensCountFlow(
+        queensCount: Int,
+        offset: Int,
+        limit: Int,
+    ): Flow<List<NQueensGamesWon>>
 }
--- app/src/main/java/com/nqueens/game/core/data/repositories/NQueensGamesWonRepository.kt
+++ app/src/main/java/com/nqueens/game/core/data/repositories/NQueensGamesWonRepository.kt
@@ -4,6 +4,8 @@ import com.nqueens.game.core.data.database.daos.NQueensGamesWonDao
 import com.nqueens.game.core.data.database.entities.NQueensGamesWon
 import com.nqueens.game.core.utils.di.IOThreadDispatcher
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -25,8 +27,36 @@ class NQueensGamesWonRepository
                 nQueensGamesWonDao.getAllGames()
             }
 
+        fun getAllGamesOffsetFlow(page: Int = 0): Flow<List<NQueensGamesWon>> =
+            nQueensGamesWonDao
+                .getAllGamesOffsetFlow(offset = PAGE_SIZE * page, limit = PAGE_SIZE)
+                .flowOn(ioThreadDispatcher)
+
+        fun getGamesOffsetByQueensCountFlow(
+            queensCount: Int,
+            page: Int = 0,
+        ): Flow<List<NQueensGamesWon>> =
+            nQueensGamesWonDao
+                .getGamesOffsetByQueensCountFlow(
+                    queensCount = queensCount,
+                    offset = PAGE_SIZE * page,
+                    limit = PAGE_SIZE,
+                ).flowOn(ioThreadDispatcher)
+
         suspend fun getGamesByQueensCount(queensCount: Int): List<NQueensGamesWon> =
             withContext(ioThreadDispatcher) {
                 nQueensGamesWonDao.getGamesByQueensCount(queensCount)
             }
+
+        fun getDistinctQueensCountsFlow(): Flow<List<Int>> =
+            nQueensGamesWonDao.getDistinctQueensCountsFlow().flowOn(ioThreadDispatcher)
+
+        suspend fun getDistinctQueensCounts(): List<Int> =
+            withContext(ioThreadDispatcher) {
+                nQueensGamesWonDao.getDistinctQueensCounts()
+            }
+
+        companion object {
+            private const val PAGE_SIZE = 30
+        }
     }
